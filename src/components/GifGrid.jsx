c008dd58346e4e170f6bc4538e095f595f0984e2
Fabricio Bencomo <fabriciobencomo.viloria@gmail.com>
2023-07-24 21:35:55 -0400

    git item component added

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react"
 import { getGifs } from "../helpers/getGifs"
+import { GifItem } from "./GifItem"
 
 export const GifGrid = ({ category }) => {
-  getGifs(category)
+  const [images, setImages] = useState([])
+  const getImages = async() => {
+    const newImages = await getGifs(category)
+    await setImages(newImages)
+    console.log(images)
+  }
+  useEffect(() => {
+    getImages()
+  }, [])
   return (
     <>
       <h1>{ category }</h1>
+      <div className="card-grid">{
+          images.map(({title, id, url}) => ( 
+            <GifItem key={id} url={url} title={title}/>
+          ))
+        }</div>
     </>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
     const API_KEY = 'GIiHFJ0YbLt8p887RWs9HlYXEx8yYmwf'
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=10`
     const resp = await fetch( url )
     const { data } = await resp.json()
     const gifs = data.map(gif => ({
@@ -8,6 +8,6 @@ export const getGifs = async (category) => {
       title: gif.title,
       url: gif.images.downsized_medium.url
     }))
-    console.log(gifs)
-    }
+    return gifs
+}
     
